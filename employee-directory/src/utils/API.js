a62a00829a19499be67e7c70a05a2fe3eb07d86f
ninobrown585 <adrian.brown1@g.austincc.edu>
2021-06-20 17:18:29 -0500

    modified code on api.js

--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const BASEURL = "https://randomuser.me/";
 
 export default {
-  search: function(query) {
-    return axios.get(BASEURL + query );
+  search: function() {
+    return axios.get(BASEURL);
   }
 };
