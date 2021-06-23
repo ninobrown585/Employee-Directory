0e8ba8562d4baf27529fd16b8d15944b40a4096d
ninobrown585 <adrian.brown1@g.austincc.edu>
2021-06-23 18:27:34 -0500

    page works..search section fix remaining1

--- employee-directory/src/Components/SearchArea.js
+++ employee-directory/src/Components/SearchArea.js
@@ -5,7 +5,7 @@ const SearchArea = (props) => {
         <div className = "d-flex justify-content-center mx-auto">
             <form>
                 <input placeholder= "Search by Name" name= "SearchArea" type= "text" 
-                  className= "form-control-lg search-font mx-auto" onChange={(event) => props.startSort(event)} />
+                  className= "form-control-lg search-font mx-auto" onChange={props.onChange} />
             </form>
         </div>
     );
--- employee-directory/src/Components/UserDirectory.js
+++ employee-directory/src/Components/UserDirectory.js
@@ -1,10 +1,10 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import CurrentEmployees from "./CurrentEmployees.js";
 import SearchArea from "./SearchArea.js";
 import API from "../utils/API";
 
 class UserDirectory extends Component {
-    state={
+    state = {
         currentemployees: [],
         searchArea: "",
         sortEmployees: [],
@@ -20,34 +20,43 @@ class UserDirectory extends Component {
         });
     };
 
+    handleChange = (event) => {
+
+        let name = event.target.value
+
+        this.setState({ searchArea: name })
+
+        console.log(name)
+    }
+
     //Sort through employees and render to screen matched search
     sortingEmps = () => {
-        let {currentemployees, searchArea} = this.state;
-        let sortEmployees = currentemployees.filter((sorted)=>{
-            return(
-                sorted.name.firstName.toLowerCase().includes(searchArea.toLowerCase()) 
+        let { currentemployees, searchArea } = this.state;
+        let sortEmployees = currentemployees.filter((sorted) => {
+            return (
+                sorted.name.firstName.toLowerCase().includes(searchArea.toLowerCase())
                 || sorted.name.lastName.toLowerCase().includes(searchArea.toLowerCase())
                 || sorted.email.toLowerCase().includes(searchArea.toLowerCase())
             );
         });
-        this.setState({ sortEmployees});
+        this.setState({ sortEmployees });
     };
 
     sorting = (event) => {
-        this.setState({ searchArea: event.target.value}, () =>{
+        this.setState({ searchArea: event.target.value }, () => {
             this.sortingEmps();
-            this.setState({sorted:true});
+            this.setState({ sorted: true });
         });
     };
 
-    render = () =>{
-        return(
+    render = () => {
+        return (
             //Header section
             <div className="header-background">
                 <div>
                     <h3>Employee Directory</h3>
                     <h2>Enter an employee name into the search area that your seeking.</h2>
-                    <SearchArea name="searchArea" sorting={this.sorting} label="SearchArea" />
+                    <SearchArea name="searchArea" sorting={this.sortingEmps} label="SearchArea" onChange={this.handleChange} />
                 </div>
                 <table>
                     <thead>
@@ -60,29 +69,29 @@ class UserDirectory extends Component {
                             {/* <th>D.O.B.</th> */}
                         </tr>
                     </thead>
-                <tbody>
-                    {
-                        //state before user enters a search
-                        !this.state.sorted ? this.state.currentemployees.map((employee) => (
-                            <CurrentEmployees key= {employee.id.value} firstName={employee.name.first}
+                    <tbody>
+                        {
+                            //state before user enters a search
+                            !this.state.sorted ? this.state.currentemployees.map((employee) => (
+                                <CurrentEmployees key={employee.id.value} firstName={employee.name.first}
 
-                                lastName={employee.name.last} phone={employee.phone} email={employee.email}
+                                    lastName={employee.name.last} phone={employee.phone} email={employee.email}
 
-                            icon={employee.picture.medium} //dob={employee.dob.date}
+                                    icon={employee.picture.medium} //dob={employee.dob.date}
 
-                                address={employee.location.street.number + " "+employee.location.street.name+ " ,"+
-                                employee.location.city+ " ," + employee.location.state+ " "+ employee.location.postcode} />
+                                    address={employee.location.street.number + " " + employee.location.street.name + " ," +
+                                        employee.location.city + " ," + employee.location.state + " " + employee.location.postcode} />
 
-                        ))
-                         : //Sorts when user enters search
-                         this.state.sortEmployees.map((employee) =>{
-                            <CurrentEmployees key={employee.id.value} firstName={employee.name.first} lastName={employee.name.last}
-                            phone={employee.phone} email={employee.email} icon={employee.picture.medium} //dob={employee.dob.date}
-                            address={employee.location.street.number + " "+ employee.location.street.name} />
-                         })  
-                    }
-                </tbody>
-              </table>
+                            ))
+                                : //Sorts when user enters search
+                                this.state.sortEmployees.map((employee) => {
+                                    <CurrentEmployees key={employee.id.value} firstName={employee.name.first} lastName={employee.name.last}
+                                        phone={employee.phone} email={employee.email} icon={employee.picture.medium} //dob={employee.dob.date}
+                                        address={employee.location.street.number + " " + employee.location.street.name} />
+                                })
+                        }
+                    </tbody>
+                </table>
             </div>
         )
     }
