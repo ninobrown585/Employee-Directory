1707adbfc875f55711e3e5db52c86e3ccb6d6e08
ninobrown585 <adrian.brown1@g.austincc.edu>
2021-06-22 19:33:32 -0500

    userdirectory complete

--- employee-directory/src/Components/UserDirectory.js
+++ employee-directory/src/Components/UserDirectory.js
@@ -42,6 +42,7 @@ class UserDirectory extends Component {
 
     render = () =>{
         return(
+            //Header section
             <div className="header-background">
                 <div>
                     <h3>Employee Directory</h3>
@@ -59,14 +60,32 @@ class UserDirectory extends Component {
                             <th>Address</th>
                         </tr>
                     </thead>
-                </table>
+                <tbody>
+                    {
+                        //state before user enters a search
+                        !this.state.sorted ? this.state.currentemployees.map((employee) =>{
+                            <CurrentEmployees key= {employee.id.value} firstName={employee.name.first}
 
+                                lastName={employee.name.last} phone={employee.phone} email={employee.email}
 
+                            icon={employee.picture.medium} dob={employee.dob.date}
+
+                                address={employee.location.street.number + " "+employee.location.street.name+ " ,"+
+                                employee.location.city+ " ," + employee.location.state+ " "+ employee.location.postcode} />
+
+                        })
+                         : //Sorts when user enters search
+                         this.state.sortEmployees.map((employee) =>{
+                            <CurrentEmployees key={employee.id.value} firstName={employee.name.first} lastName={employee.name.last}
+                            phone={employee.phone} email={employee.email} icon={employee.picture.medium} dob={employee.dob.date}
+                            address={employee.location.street.number + " "+ employee.location.street.name} />
+                         })  
+                    }
+                </tbody>
+              </table>
             </div>
         )
     }
-
-
 }
 
 export default UserDirectory;
